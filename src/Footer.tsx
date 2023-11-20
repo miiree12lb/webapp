501316652ca9370aa9c7b6ed07ba429b5fd2499f
Mireia <66952824+miiree12lb@users.noreply.github.com>
2023-11-20 21:59:40 +0100

    change homepage route to deploy

---
+++ src/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+//@ts-ignore
+import instagram from "../assets/images/socials/instagram.svg";
+//@ts-ignore
+import linkedin from "../assets/images/socials/linkedin.svg";
+//@ts-ignore
+import linktree from "../assets/images/socials/linktree.png";
+// @ts-ignore
+import logo from "../assets/images/logo.png";
+
+export default function Footer(){
+    return (
+        <div>
+            <Link to="https://linktr.ee/miiree12lb"><img alt="linktree" src={linktree} width="50" height="50" /></Link>
+
+            <Link to="https://www.instagram.com/miiree12lb/"><img alt="instagram" src={instagram} width="50" height="50" /></Link>
+
+            <Link to="https://www.linkedin.com/in/mireia-lopez-bruch-0b7768241/"><img alt="linkedin" src={linkedin} width="50" height="50" /></Link>
+        </div>
+    );
+}
